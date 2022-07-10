f8c7445f2956b4e2bc2ef3cf711905dd29e57abb
Finlay Pearson <fjnlpy@gmail.com>
2022-07-10 23:12:42 +0100

    Add file and REPL inputs to compiler

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,68 @@
+#include <string>
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+
+#include "utils/utils.hpp"
+
+namespace fs = std::filesystem;
+
+void
+run(const std::string &program)
+{
+  LOGD("The input is ", program);
+}
+
+void
+runPrompt()
+{
+  std::string input;
+  while (std::cout << "> ", getline(std::cin, input)) {
+    try {
+      run(input);
+    } catch (...) {
+      // TODO: actual error reporting.
+      LOGE("Error with input: ", input);
+    }
+  }
+}
+
+void
+runFile(const char *fileName)
+{
+  std::ifstream fileStream(fileName);
+  if (!fileStream.good()) {
+    LOGE("Could not open for I/O: ", fileName);
+    return;
+  }
+
+  std::stringstream stringStream;
+  stringStream << fileStream.rdbuf();
+
+  try {
+    run(stringStream.str());
+  } catch (...) {
+    // TODO: actual error reporting.
+    LOGE("Error in file: ", fileName);
+    // Should use non-zero exit code in this case.
+    exit(-1);
+  }
+}
 
 int
-main(void)
+main(int argc, char **argv)
 {
-  std::cout << "Hello world." << std::endl;
+  if (argc == 1) {
+    runPrompt();
+  } else if (argc == 2) {
+    runFile(argv[1]);
+  } else {
+    LOGI(
+R"(
+Pass pass the path to the file to be interpreted, or nothing if you want to use
+the interactive prompt.
+)"
+    );
+    return -1;
+  }
 }
