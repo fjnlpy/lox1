43b5866594a58f39828013a29d05b6d73fdd0b6e
Finlay Pearson <fjnlpy@gmail.com>
2022-07-12 23:27:04 +0100

    Add layout of `lex` method

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -21,4 +21,29 @@ Token::Token(Type tokenType, unsigned lineNumber, std::string contents)
 
 Lexer::Lexer() =default;
 
+std::vector<Token>
+Lexer::lex(const std::string &sourceCode)
+{
+  sourceCode_ = std::stringstream(sourceCode);
+  // Reset state from last call (if any).
+  tokens_.clear();
+  currentLine_ = 1;
+
+  while (sourceCode_) {
+    // TODO: do lexing
+  }
+
+  if (!sourceCode_.eof()) {
+    // Must have had an error with the stream. Can't do any more lexing, so fail.
+     // TODO: more info?
+     // TODO: have a utils::compiler_error instead?
+    throw std::runtime_error("Unable to read from source code input stream.");
+  }
+
+  // TODO: should you always use emplace instead of move?
+  tokens_.push_back(Token(Token::Type::EOFF, currentLine_));
+  // TODO: I think move is correct here because tokens_ isn't a local var -- check effective cpp book.
+  return std::move(tokens_);
+}
+
 }
