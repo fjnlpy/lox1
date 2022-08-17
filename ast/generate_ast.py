e1568142ceb78d5539b93457d3999bfd5ef44ec4
Finlay Pearson <fjnlpy@gmail.com>
2022-08-17 22:40:28 +0100

    Emit Visitor interface

--- ast/generate_ast.py
+++ ast/generate_ast.py
@@ -8,6 +8,7 @@ def emit(classes, output_dir):
     # TODO: Read the JSON and emit C++ structs to `output dir/<baseClass>.hpp`
 
     base_class = classes["baseClass"]
+    subclass_names = classes["subClasses"].keys()
 
     output_dir = Path(output_dir)
     assert output_dir.exists() and output_dir.is_dir(), f"Output dir is not valid directory: {output_dir}"
@@ -16,9 +17,10 @@ def emit(classes, output_dir):
     assert not output_file.exists(), f"Output file already exists: {output_file}"
 
     includes_snippet = make_includes_snippet()
+    visitor_snippet = make_visitor_snippet(subclass_names)
     base_snippet = make_base_class_snippet(base_class)
 
-    full_class = "".join([includes_snippet, base_snippet]) + "\n"
+    full_class = "".join([includes_snippet, visitor_snippet, base_snippet]) + "\n"
 
     with open(output_file, "x") as writer:
       writer.write(full_class)
@@ -26,13 +28,39 @@ def emit(classes, output_dir):
 def make_includes_snippet():
   # We have a known set of includes
   return """
-  #include <string>
-  """
+#include <string>
+"""
+
+def make_visitor_snippet(subclass_names):
+  top = f"""
+template <class T>
+class Visitor {{
+public:
+  virtual ~Visitor() =default;
+
+"""
+
+  subclass_methods = []
+  for c in subclass_names:
+    subclass_methods.append(f"  virtual T visit{c}(const {c} &{to_camel_case(c)}) =0;")
+    subclass_methods.append(f"  virtual T visit{c}({c} &{to_camel_case(c)}) =0;\n")
+
+  return top + "\n".join(subclass_methods) + "\n};\n"
 
 def make_base_class_snippet(base_class_name):
-  return """
-  // TODO: emit base class.
-  """
+  return f"""
+struct {base_class_name} {{
+  template <class T>
+  T
+  accept()
+}};
+"""
+
+def to_camel_case(pascal_case_word):
+  if len(pascal_case_word) > 0 and pascal_case_word[0].isupper():
+    return pascal_case_word[0].lower() + pascal_case_word[1:]
+  else:
+    return pascal_case_word
 
 def main():
   if len(argv) != 3:
