8320e203ffcb3e24fda92ecb8a7b957658771e4a
Finlay Pearson <fjnlpy@gmail.com>
2022-10-06 22:43:49 +0100

    Add test helpers for asserting on ASTs

--- test/parser/ParserTests.cpp
+++ test/parser/ParserTests.cpp
@@ -2,10 +2,57 @@
 
 #include <variant>
 #include <vector>
+#include <sstream>
 
 #include "parser/Parser.h"
+#include "visit/PrettyPrinter.hpp"
 
 using lexer::Token;
+using namespace ast;
+
+namespace {
+
+void
+assertProbablyTheSame(Expr &e1, Expr &e2)
+{
+  // We could write a pass which traverses two expressions and checks
+  // that they're structurally the same. But I don't think we quite need
+  // that right now. An easier approach is just to print them out
+  // and say that they're the same iff they look the same.
+  // (This also gives us nice assertion messages for free:).)
+  // Possible sources of incorrectness: rounding of numbers, ...
+  visit::PrettyPrinter printer;
+  auto output1 = printer.print(e1);
+  auto output2 = printer.print(e2);
+  ASSERT_EQ(output1, output2);
+}
+
+void
+assertDoesNotCompile(const std::vector<Token> &tokens)
+{
+  parser::Parser parser;
+  try {
+    parser.parse(tokens);
+
+    std::stringstream stream;
+    stream << "Expected the sequence of tokens to fail parsing: ";
+    if (tokens.size() == 0) {
+      stream << "{}";
+    } else {
+      stream << "{\n  " << tokens[0];
+      for (auto it = ++tokens.begin(); it != tokens.end(); ++it) {
+        stream << ",\n  " << *it;
+      }
+      stream << "\n}";
+    }
+    FAIL() << stream.str();
+  } catch (const CompileError &error) {
+    // Test passed!
+    //LOGD(e.what());
+  }
+}
+
+}
 
 TEST(ParserTests, TestPrimaryExpression) {
   parser::Parser parser;
